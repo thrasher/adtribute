4fa88d4fb5fd983c53b112a24ae65ccf469a81dd
Jason Thrasher <thrasher@brightroll.com>
2014-07-23 11:24:08 -0700

    Branded images for BR

---
+++ make-icons.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# brute force replace all images with properly sized BrightRoll version
+# see: http://www.ainotenshi.org/818/resizing-images-using-the-command-line
+
+FILES=`find icons -type f`
+
+for FILE in $FILES ; do
+	#echo "foudn file $FILE"
+	HEIGHT=`sips --getProperty pixelHeight $FILE | grep -v png | sed "s/  pixelHeight: \([0-9]*\)/\1/g"`
+	WIDTH=`sips --getProperty pixelWidth $FILE | grep -v png | sed "s/  pixelWidth: \([0-9]*\)/\1/g"`
+	cp br.png.bak br.png
+	echo "height width: $HEIGHT $WIDTH"
+	sips -z $HEIGHT $WIDTH br.png
+	mv br.png $FILE
+done
